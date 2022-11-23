4ea711390a4e208c99029c5968b5a392f7bf2d78
Ray Deck <ray@raydeck.com>
2022-11-23 09:32:02 -0500

    fix: loading

--- src/Loading.tsx
+++ src/Loading.tsx
@@ -2,9 +2,13 @@ import { FC } from "react";
 import Logo from "./logo.png";
 const Loading: FC = () => {
   return (
-    <div className="w-full h-full fixed block top-0 left-0 bg-blue-600 opacity-75 z-50">
-      <div className="text-green-500 opacity-75 top-1/2 my-0 mx-auto block relative w-0 h-0">
-        <img src={Logo} alt="State Change Pro Loading..." />
+    <div className="w-full bg-white align-center justify-around flex">
+      <div className="p-6 ">
+        <img
+          className="h-8 w-8 animate-pulse"
+          src={Logo}
+          alt="State Change Pro Loading..."
+        />
       </div>
     </div>
   );
