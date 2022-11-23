45ca209f6b0af8cc5a9beb84c8662e1f1712073f
Ray Deck <ray@raydeck.com>
2022-11-22 22:23:46 -0500

    feat: new loading

--- src/Loading.tsx
+++ src/Loading.tsx
@@ -2,13 +2,10 @@ import { FC } from "react";
 import Logo from "./logo.png";
 const Loading: FC = () => {
   return (
-    <div className="w-full h-full fixed block top-0 left-0 bg-white opacity-75 z-50">
-      <span
-        className="text-green-500 opacity-75 top-1/2 my-0 mx-auto block relative w-0 h-0"
-        style={{ top: "50%" }}
-      >
+    <div className="w-full h-full fixed block top-0 left-0 bg-blue-600 opacity-75 z-50">
+      <div className="text-green-500 opacity-75 top-1/2 my-0 mx-auto block relative w-0 h-0">
         <img src={Logo} alt="State Change Pro Loading..." />
-      </span>
+      </div>
     </div>
   );
 };
